542229fca2d18cff9c945b6916e29d8d4499f0a4
Brett Henderson <bretth18@gmail.com>
2016-01-08 10:57:56 -0800

    update ssl to fix mixed content
    sluts love mixed content

--- public/index.js
+++ public/index.js
@@ -1,21 +1,21 @@
 // shoot js when DOM is loaded
 $(document).ready(function() {
-  
+
     // setting global variables
     var recentTrackUrl = "https://ws.audioscrobbler.com/2.0?method=user.getRecentTracks&user=bretth18&limit=1&api_key=6e7c0a29cd508f42a6737e5fd3d6110b&format=json"
     var defaultCoverImage = "http://cdn.last.fm/flatness/responsive/2/noimage/default_album_300_g4.png"
     var refreshTime = 1000 //look for updated JSON every "xxx" miliseconds
-    
-    //definig the function which gets JSON data from the last.fm api and sets it to the 
+
+    //definig the function which gets JSON data from the last.fm api and sets it to the
     function setRecentTrack() {
         $.getJSON(recentTrackUrl, function(data){
-            var info = data.recenttracks.track[0]; 
-                var track = info["name"]; 
-                var artist = info.artist["#text"]; 
-                var album = info.album["#text"]; 
-                var cover = info.image[3]["#text"]; 
+            var info = data.recenttracks.track[0];
+                var track = info["name"];
+                var artist = info.artist["#text"];
+                var album = info.album["#text"];
+                var cover = info.image[3]["#text"];
                 var url = info["url"];
-            
+
             if ( cover.length > 0 ) {
                    $(".cover").attr("src", cover)
                 }
@@ -29,10 +29,10 @@ $(document).ready(function() {
                 $(".track").html(track)
                 $(".album").html(album)
             });
-    } 
+    }
     //run function on pageload then load on time interval
     setRecentTrack();
-    setInterval(setRecentTrack, refreshTime);       
+    setInterval(setRecentTrack, refreshTime);
 
-    
+
 })
